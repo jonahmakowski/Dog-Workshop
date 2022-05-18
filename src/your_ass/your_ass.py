a7513312879feff5254586c6f83e54860419e633
jonahmakowski <jonah@makowski.ca>
2022-05-18 17:45:46 -0400

    Noah made this when he was really annoyed with me

---
+++ src/your_ass/your_ass.py
@@ -0,0 +1,28 @@
+import time
+
+print("Noah: Jonah is ass")
+print("Noah: Jonah is ass")
+print("Noah: Jonah is ass")
+print("Noah: Jonah is ass")
+print("Noah: Jonah is ass")
+print("Noah: Jonah is ass")
+print("Noah: Jonah is ass")
+print("Noah: Jonah is ass")
+print("Noah: Jonah is ass")
+print("Noah: Jonah is ass")
+
+time.sleep(3)
+
+print('Jonah: I know you are, but what am I?')
+
+time.sleep(3)
+
+print("Noah: a ass")
+
+time.sleep(3)
+
+while True:
+    print('Jonah: I know you are, but what am I?')
+    print("Noah: JONAH IS THE BIGGEST ASS EVER")
+
+    time.sleep(0.1)
